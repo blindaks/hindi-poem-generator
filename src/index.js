5b6dc4d0616ff607d24e313b788210aa98bd74fc
Akrati Verma <akrativerma2002@gmail.com>
2024-07-26 01:23:59 +0530

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let key = "07f2db5d81d18450d2d162fbo30aat29";
   let context =
-    "You are a romantic poem expert and love to write songs. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem.";
+    "You are a poem expert and love to write songs. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem.";
   let prompt = `User instructions: Generate a Hindi poem about ${instructionsInput.value}`;
   let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Hindi poem about ${instructionsInput.value}</div>`;
 
   axios.get(url).then(displayPoem);
 }
