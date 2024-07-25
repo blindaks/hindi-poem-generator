11e7e02be2a1057bcf34ca21cde4b931f28d2fc4
Akrati Verma <akrativerma2002@gmail.com>
2024-07-26 02:43:48 +0530

    Made changes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let key = "07f2db5d81d18450d2d162fbo30aat29";
   let context =
-    "You are a poem expert and love to write songs. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem.";
+    "You are a poem expert and love to write poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem.";
   let prompt = `User instructions: Generate a Hindi poem about ${instructionsInput.value}`;
   let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
