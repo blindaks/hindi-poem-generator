fe344dbf91cbadde3115dfb19bb372736f6a6423
Akrati Verma <akrativerma2002@gmail.com>
2024-07-25 20:59:00 +0530

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "hehehhhe hgejgwejw jwhjhj",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let key = "07f2db5d81d18450d2d162fbo30aat29";
+  let context =
+    "You are a romantic poem expert and love to write songs. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem.";
+  let prompt = `User instructions: Generate a Hindi poem about ${instructionsInput.value}`;
+  let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  axios.get(url).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
